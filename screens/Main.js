891fede3780f67534e341fd1ca4bedd435304bd3
Fun Planet <dipamdiptam@gmail.com>
2021-12-23 19:10:31 +0530

    Created a new Expo app

---
+++ screens/Main.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import * as Permissions from "expo-permissions";
+import * as FaceDetector from "expo-face-detector";
